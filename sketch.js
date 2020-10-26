b88ec5ba2e10290a42a733d37b9c02542d8860eb
Pratyush21706 <65002484+Pratyush21706@users.noreply.github.com>
2020-10-26 16:06:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,55 +8,121 @@ colors.pink = #f73057
 
 */
 
-const STAGE = 0;
+const STAGE = 1;
+var wt = 96,ag = 1;
 
-function preload(){
-    
+function preload() {
+
 }
-function setup(){
+
+function setup() {
     noCanvas();
-  male = createDiv(``);
+    male = createDiv(``);
     male.addClass(`gender1`);
-    male.position(8,10)
-    
+    male.position(8, 10)
+
     female = createDiv(``);
     female.addClass(`gender2`);
-    female.position(width/1.85,10)
-    
+    female.position(width / 1.89, 10)
+
     height = createDiv(``);
-        height.addClass(`height`);
-    height.position(8,width/1.87)
+    height.addClass(`height`);
+    height.position(8, width / 1.87)
+
+    tit = createP(`HEIGHT`);
+    tit.parent(height)
+    tit.addClass(`para`);
+//    
+      
     
-     weight = createDiv(``);
+    weight = createDiv(``);
     weight.addClass(`pro`);
-    weight.position(8,window.innerHeight/1.55)
-    
+    weight.position(8, window.innerHeight / 1.55)
+
     age = createDiv(``);
     age.addClass(`pro`);
-    age.position(width/1.85,window.innerHeight/1.55);
+    age.position(width / 1.89, window.innerHeight / 1.55);
+
+    tit1 = createP(`WEIGHT`);
+    tit1.parent(weight)
+    tit1.addClass(`para1`);
     
-    sld = createSlider();
+    tit2 = createP(`AGE`);
+    tit2.parent(age)
+    tit2.addClass(`para2`);
+    
+    sld = createSlider(10, 180, 10);
     sld.parent(height);
     sld.addClass(`sld`)
-    
+
     submit = createButton(`Calculate`);
-    submit.position(0,window.innerHeight/1.09);
-//    submit.addClass(`button`)
+    submit.position(0, window.innerHeight/1.09);
+    submit.addClass(`check`)
+    
+    plus = createButton("+");
+    plus.addClass(`math`);
+    plus.parent(weight)
+    plus.position(width/4,height);
+    plus.mousePressed(add)
+    
+    minus = createButton("-");
+    minus.addClass(`math`);
+    minus.parent(weight)
+    minus.position(width/10,height);
+        minus.mousePressed(sub)
+    
+    plus1 = createButton("+");
+    plus1.addClass(`math`);
+    plus1.parent(age)
+    plus1.position(width/4,height);
+    plus1.mousePressed(add)
+    
+    minus1 = createButton("-");
+    minus1.addClass(`math`);
+    minus1.parent(age)
+    minus1.position(width/9,height);
+        minus1.mousePressed(sub)
+
+
+    //    ageI = createInput(`0`).attribut
+    //    submit.addClass(`button`)
 }
-function draw(){
-    if(STAGE ===0){
-//        icon.position(width/2,height);
-        
+function add(){
+    if(wt<99){
+    wt++;
     }
-    if(STAGE===1){
-            male.position(8,10)
-    female.position(width/1.85,10)
-    height.position(8,width/1.87)
-    weight.position(8,window.innerHeight/1.55)
-    age.position(width/1.85,window.innerHeight/1.55);
-    submit.position(0,window.innerHeight/1.09);
+}
+function sub(){
+    wt--;
+}
+function draw() {
+    if (STAGE === 0) {
+        //        icon.position(width/2,height);
 
     }
-    
-    drawSprites();
+    if (STAGE === 1) {
+        male.position(8, 10)
+        female.position(width / 1.85, 10)
+        height.position(8, width / 1.87)
+        weight.position(8, window.innerHeight / 1.55)
+        age.position(width / 1.85, window.innerHeight / 1.55);
+        submit.position(0, window.innerHeight / 1.09);
+        let ht = sld.value();
+        no = createP(ht);
+        no.parent(height)
+        no.position(width / 2.5, height / 4)
+        no.addClass(`no`);
+        
+         no1 = createP(wt);
+        no1.parent(weight)
+        no1.position(width /6, height /-6)
+        no1.addClass(`no1`);
+        
+          no2 = createP("15");
+        no2.parent(age)
+        no2.position(width /5.5, height /6)
+        no2.addClass(`no1`);
+    }
+
+
 }
